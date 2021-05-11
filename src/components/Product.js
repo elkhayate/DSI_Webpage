5af976d7e1555539b674dda46e9c72b9f7c3d898
elkhayate <melkhayate61@gmail.com>
2021-05-12 00:27:47 +0300

    Adding product component

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,8 +1,25 @@
 import React from 'react';
-
-
-
+import style from "./product.module.css";
+import ppana from "../images/ppana.png";
+import line from "../images/line.png"
+import Footer from "../components/Footer";
 function Product() {
-    return(<h1>Product page.</h1>)
+    return(
+        <section className={style.product}>
+            <div className={style.container1}>
+                <h1 className={style.title}>The most advanced technology <span className={style.span}>always work hand-in-hand with qualified people</span></h1>
+            </div>
+            <div className= {style.container2}>
+                <div className={style.content}>
+                <h1>Our Product</h1>
+                <img src={line} alt="" />
+                <p>Our product is made on the base of our team’s careful research and analyses, ranging from internet based application such as Internet Banking; mobile frontend application such as Mobile Banking; transaction based banking such as EDC Banking; and other financial platforms such as Personal Investment Portfolio.</p>
+                <p>Our product and service range will technically safeguard your business operations through continued application development. It is all topped with our integrated procedure made in accordance with the industry’s best practices, which will drive the built-in system towards the creation of secured and value added services for you as business partners.</p>
+                </div>
+                <img className={style.ppana} src={ppana} alt = ""/>
+            </div>
+            <Footer />
+        </section>
+    )
 }
 export default Product;
--- src/components/Services.js
+++ src/components/Services.js
@@ -1,8 +1,32 @@
 import React from 'react';
-
-
-
+import style from "./services.module.css";
+import Footer from "../components/Footer";
+import line from "../images/line.png";
+import pana from "../images/spana.png";
 function Services() {
-    return(<h1>Services page.</h1>)
+    return(
+        
+        <section className = {style.services}>
+            <div className={style.container1}>
+                <h1>Just a smile away from    <span>Winning your customer’s trust</span></h1>
+            </div>
+            <div className={style.container2}>
+                <div className={style.container2content}>
+                    <h1>Our Service</h1>
+                    <img className={style.line} src={line}alt = ""/>
+                    <p><span>DSI</span>’s shared service solutions focus on the front-end based software development, designed specifically for the banking and financial sectors. We short-cut your front-liners to customers interaction process through a way that mediates your business process in a more direct communication means, in just a smile away from winning your customer’s trust.</p>
+                    <p>Through close partnership, we always work together with your team, all throughout from pretopost project cycles – initial planning, project development, implementation, evaluation up to reporting documentation – to fully coach you so that all your specific business.</p>
+                    <p>requirements can be delivered at the right time to the right audience of particular interests. Moreover, our team will support you to provide a detailed consultation throughout operation stage, and after the project implementation cycles by applying specific monitoring and improvement methodology. Our experienced people will drive your personalized service, by utilizing the best technology supports no other businesses can provide.</p>
+                </div>
+                <img className = {style.spana}src={pana} alt = ""/>
+            </div>
+
+            
+            <Footer />
+            
+        </section>
+        
+        
+    )
 }
 export default Services;
