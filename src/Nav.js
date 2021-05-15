9b3da9cfd588d1595cff0acfe893826c181c165c
elkhayate <melkhayate61@gmail.com>
2021-05-15 06:46:15 +0300

    structure

--- src/Nav.js
+++ src/Nav.js
@@ -1,37 +1,47 @@
-import React from 'react';
+import React,{useState} from 'react';
 import logo from "./images/logo.png";
 import{Link} from "react-router-dom";
 import style from "./Nav.module.css";
 import union from "./images/Union.png";
 import shuffle from "./images/shuffle.png";
 import mail from "./images/mail.png";
+import arrow from "./images/chevron_down.png";
+
+
 function Nav() {
-    const Style = {textDecoration: 'none'};
+
+    const [showAbout,setShowAbout] = useState(false);
+
+    const Style = {textDecoration: 'none', cursor: "pointer"};
     return(
         <nav className={style.nav}>
-            <Link to ="/" style={Style}>
+            <Link to ="/" style={Style} onClick={()=>setShowAbout(false)}>
             <img className={style.logo} src={logo} alt = "logo"/>
             </Link>
             <ul className={style.navlinks}>
-                <Link style={Style} to ="/services">
+                <Link style={Style} to ="/services" onClick={()=>setShowAbout(false)}>
                 <li>Services</li>
                 </Link>
-                <Link style={Style} to = "/product">
+                <Link style={Style} to = "/product" onClick={()=>setShowAbout(false)}>
                 <li>Product</li>
                 </Link>
-                <Link style={Style} to = "/technology">
+                <Link style={Style} to = "/technology" onClick={()=>setShowAbout(false)}>
                 <li>Technology</li>
                 </Link>
-                <Link style={Style} to = "/about">
-                <li>About</li>
-                </Link>
-                <Link style={Style} to = "/abouts">
-                <li>Abouts</li>
-                </Link>
-                <Link style={Style} to = "/client">
+                
+                <li style={Style} className={style.about} onClick={()=>setShowAbout(!showAbout)} >About  <img src={arrow} alt=""/></li>
+                {
+                    showAbout && (<div className={style.dropDown}  >
+                        <p style={{margin:12}} ><Link style={Style} className={style.drop} to = "/about" onClick={()=>setShowAbout(false)}>About us</Link></p>  
+                        <p style={{margin:12}}> <Link style={Style} className={style.drop} to = "/abouts" onClick={()=>setShowAbout(false)}>Team</Link></p>
+                       
+                    </div>)
+                }
+                
+                <Link style={Style} to = "/client" onClick={()=>setShowAbout(false)}>
                 <li>Client</li>
                 </Link>
-                <Link style={Style} to = "/partner">
+                <Link style={Style} to = "/partner" onClick={()=>setShowAbout(false)}>
                 <li>Partner</li>
                 </Link>
                 
--- src/components/Client.js
+++ src/components/Client.js
@@ -1,8 +1,62 @@
 import React from 'react';
-
-
+import style from "./client.module.css";
+import cpana from "../images/cpana.png"
+import Footer from "./Footer";
+import line from "../images/line.png";
+import b1 from "../images/image 17.png";
+import b2 from "../images/image 16.png";
+import b3 from "../images/image 18.png";
 
 function Client() {
-    return(<h1>Client page.</h1>)
+    return(
+        <section className={style.client}>
+            <div className={style.container}>
+                <h1>Taking our client in more<br/> comfortable way<br/> <span>Deliver IT around  the globe</span></h1>
+                <img src={cpana} alt=""/>
+            </div>
+            <div className={style.test}/>
+            <div className={style.container1}>
+                <h1>Client Line-Up</h1>
+                <img src={line} alt=""/>
+            </div>
+            <div className={style.container2}>
+                <img src={b1} alt=""/>
+                <div className={style.content}>
+                    <h1>Bank Tabungan Negara</h1><br/>
+                    <h2><span>Project WB-Bank BTN :</span></h2><br/>
+                    <p> Application Development Project Teller System or known as Web Applications Branch for Bank BTN. Project is to replace the existing application at Bank BTN is running under the Windows platform or desktop applications into Web-based applications applications.</p><br/>
+                    <p>Branch Web application will be implemented throughout the Bank BTN - Indonesia, about 600 BTN Office.</p><br/>
+                    <p>Web framework: tapestry 5.3<br/>Back end : spring 3.1 + jpa 2<br/>Security: apache shiro<br/>Database: ibm db2 9.7</p>
+                </div>
+            </div>
+            <div className={style.container3}>
+                <img src={b2} alt=""/>
+                <div className={style.content}>
+                    <h1>Bank BPR KS</h1><br/>
+                    <h2><span>Mobile Banking :</span></h2>
+                    <p>This project provides delivery channel application over mobile device for customer to perform banking and financial transactions, such as inquiry balance, overbook, purchase, and payment. Using: JAVA technologies, PostgreSQL, Android technologies, iOS technologies, Blackberry technologies</p><br/>
+                    <h2><span>Soft Token :</span></h2>
+                    <p>This project provides software application to generate token secure code from mobile device used to authenticate banking and financial transaction from delivery channel performed by customer. Using: JAVA technologies, PostgreSQL, Android technologies, iOS technologies, Blackberry technologies</p><br/>
+                    <h2><span>Internet Banking :</span></h2>
+                    <p>This project provides delivery channel application over web browser for customer to perform banking and financial transactions, such as inquiry balance, overbook, purchase, and payment. Using: JAVA technologies, PostgreSQL</p><br/>
+                    <h2><span>KIOSK :</span></h2>
+                    <p>This project provides non-cash delivery channel machine and software application for customer to perform banking and financial transactions, such as inquiry balance, overbook, purchase, and payment. Using: JAVA technologies, .NET framework, PostgreSQL</p><br/>
+                </div>
+            </div>
+            <div className={style.container4}>
+                <img src={b3} alt=""/>
+                <div className={style.content}>
+                <h1>Bank BPR KS</h1><br/>
+                    <h2><span>Mobile Banking :</span></h2>
+                    <p>This project provides delivery channel application over mobile device for customer to perform banking and financial transactions, such as inquiry balance, overbook, purchase, and payment. Using: JAVA technologies, PostgreSQL, Android technologies, iOS technologies, Blackberry technologies</p><br/>
+                    <h2><span>Soft Token :</span></h2>
+                    <p>This project provides software application to generate token secure code from mobile device used to authenticate banking and financial transaction from delivery channel performed by customer. Using: JAVA technologies, PostgreSQL, Android technologies, iOS technologies, Blackberry technologies</p><br/>
+                    <h2><span>Internet Banking :</span></h2>
+                    <p>This project provides delivery channel application over web browser for customer to perform banking and financial transactions, such as inquiry balance, overbook, purchase, and payment. Using: JAVA technologies, PostgreSQL</p><br/>
+                </div>
+            </div>
+            <Footer />
+        </section>
+    )
 }
 export default Client;
